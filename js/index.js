613b5aea18eba95059bbdcf99de0b3b2174fdcb6
Oltinbek2010 <oltinbek04@gmail.com>
2024-06-05 16:10:27 +0500

    commit

---
+++ js/index.js
@@ -0,0 +1,323 @@
+// let A = +prompt ("A sonini kiriting");
+// let res = '';
+// if (A > 0) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let res = '';
+// if (A % 2 == 1) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let res = '';
+// if (A % 2 == 1) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let res = '';
+// if (A % 2 == 1) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let res = '';
+// if (A > 2 && B <= 3) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let res = '';
+// if (A >= 0 || B < -2) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let C = +prompt ("C sonini kiriting");
+// let res = '';
+// if (A <= B && B <= C) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let C = +prompt ("C sonini kiriting");
+// let res = '';
+// if (A < B && B < C || A > B && B > C) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let res = '';
+// if (A % 2 == 1 && B % 2 == 1) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let res = '';
+// if (A % 2 == 1 || B % 2 == 1) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let res = '';
+// if (A % 2 == 1 && B % 2 == 0 || A % 2 == 0 && B % 2 == 1 ) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let res = '';
+// if (A % 2 == 1 && B % 2 == 1 || A % 2 == 0 && B % 2 == 0 ) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let C = +prompt ("C sonini kiriting");
+// let res = '';
+// if (A > 0 && B > 0 && C > 0) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let C = +prompt ("C sonini kiriting");
+// let res = '';
+// if (A > 0 || B > 0 || C > 0) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let C = +prompt ("C sonini kiriting");
+// let res = '';
+// if (A > 0 && B < 0 && C < 0 || A < 0 && B > 0 && C < 0 || A < 0 && B < 0 && C > 0) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let C = +prompt ("C sonini kiriting");
+// let res = '';
+// if (A > 0 && B > 0 && C < 0 || A > 0 && B < 0 && C > 0 || A < 0 && B > 0 && C > 0) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let onlik = Math.trunc(A / 10);
+// let birlik = A % 10;
+// let res = '';
+// if ( A == onlik * 10 + birlik && A % 2 == 0) {
+//     res = "True"
+// }else{
+//     res = "False"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let res = '';
+// if (A > 0) {
+//     res = "A+=1"
+// }else{
+//     res = ""
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let res = '';
+// if (A > 0) {
+//     res = "A += 1"
+// }else{
+//     res = "A -= 2"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let res = '';
+// if (A > 0) {
+//     res = "A += 1"
+// }if (A == 0) {
+    //    res = "10"
+// }
+// else{
+//     res = "A -= 2"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let C = +prompt ("C sonini kiriting");
+// let res = '';
+// if (A > 0) {
+//     res = "res + 1"
+// }
+// if (B > 0) {
+//     res = "res + 1"
+// }// if (C > 0) {
+//     res = "res + 1"
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let C = +prompt ("C sonini kiriting");
+// let res2 = '';
+// let res = '';
+// if (A > 0) {
+//     res = "res + 1"
+// }else {
+//     res2 = (res2+1)
+// }
+// if (B > 0) {
+//     res = "res + 1"
+// }else {
+//     res2 = (res2+1)
+// }
+
+//  if (C > 0) {
+//     res = "res + 1"
+// }else{
+//        res2 = (res2+1)
+// }
+// console.log(res, res2);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let res = '';
+// if (A > B) {
+//     res = A
+// }else{
+//     res = B
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let res = '';
+// let nat = '';
+// if (A > B) {
+//     res = A
+// }if (A < B) {
+//     nat = B
+// }
+// console.log(res, nat);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let res = '';
+// if (A < B) {
+//     res = A + B
+// }else{
+
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let res = '';
+// if (A < B) {
+//     res = A + B
+// }else{
+
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let res = '';
+// if (A = B) {
+//     res = 0
+// }else {
+//     res = A + B
+// }
+// console.log(res);
+
+// let A = +prompt ("A sonini kiriting");
+// let B = +prompt ("B sonini kiriting");
+// let C = +prompt ("C sonini kiriting");
+// let res = '';
+// if (A < B && B < C) {
+//     res = A
+// }if (A < B && B > C){
+// res = C
+// }if (A > B && B < C){
+// res = B
+// }
+// console.log(res);
+
+let A = +prompt ("A sonini kiriting");
+let B = +prompt ("B sonini kiriting");
+let C = +prompt ("C sonini kiriting");
+let res = '';
+if (A < B && B < C) {
+    res = B
+}if (B < A && A < C){
+res = B
+}if (A < C && C < B){
+res = C
+}if (A > B && B > C) {
+    res = B
+}if (B > A && A > C){
+res = B
+}if (A > C && C > B){
+res = C
+}
+console.log(res);
